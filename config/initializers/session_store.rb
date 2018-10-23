d558f640711c3451129d34e39dd7c3f69c6aaea0
Junyan Yang <yangwu123789@gmail.com>
2018-10-23 17:30:18 -0500

    initiate
    trying to initiate the project

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_https---github_com-UIOWASELT2018-selt18project-g012_git_session'
